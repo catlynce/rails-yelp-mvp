82eeab6cd05e430a60ff90c47b4b66828bc46ab6
Nuno Martins <nuno@alvesmartins.eu>
2020-02-01 12:05:35 +0000

    Refactor inclusion to range

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   validates :content, :rating, presence: true
-  validates :rating, numericality: { only_integer: true }, inclusion: { in: [0,1,2,3,4,5] }
+  validates :rating, numericality: { only_integer: true }, inclusion: { in: 0..5 }
   belongs_to :restaurant
 end
